60afd6516bbbc18fc0248fe248b400550cfe6f16
Justin Walsh <contact.me@thejustinwalsh.com>
2021-01-11 16:22:02 -0700

    feat(s5): State in React Components

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,16 +1,36 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-class App extends Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
 
-    return <div>Latitude: </div>;
+class App extends Component {
+  state = {
+    lat: null,
+    errorMessage: null,
+  };
+
+  constructor(props) {
+    super(props);
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading</div>;
   }
 }
-
-export default App;
 
 ReactDOM.render(<App />, document.querySelector("#root"));
