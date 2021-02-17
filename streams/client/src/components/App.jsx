21684860323b3ddffbe5fb45b6a829813b0c3cfb
Justin Walsh <justinw@mortgage365.com>
2021-02-16 18:28:03 -0700

    feature(s25.5): CRUD front-end complete

--- streams/client/src/components/App.jsx
+++ streams/client/src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 
 import history from "../history";
 
@@ -7,7 +7,7 @@ import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamList from "./streams/StreamList";
-import { StreamShow } from "./streams/StreamShow";
+import StreamShow from "./streams/StreamShow";
 
 export const App = () => {
   return (
@@ -15,11 +15,13 @@ export const App = () => {
       <Router history={history}>
         <div>
           <Header />
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/edit/:id" exact component={StreamEdit} />
-          <Route path="/streams/delete/:id" exact component={StreamDelete} />
-          <Route path="/streams/show/:id" exact component={StreamShow} />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+          </Switch>
         </div>
       </Router>
     </div>
